7479d972e55562d26ad0f07aadccf7cbc25b4b61
Olga Kotova <oliktva@gmail.com>
2016-06-26 22:35:23 +0300

    Add map

--- js/script.js
+++ js/script.js
@@ -15,53 +15,22 @@
       iconMenu.classList.toggle("active");
     });
 
-  /*       plusAdults.addEventListener("click", function(event) {
-        event.preventDefault();
-        var value = parseInt(inputAdults.value) + 1;
-        inputAdults.value = value;
-      });
+    //----------------------------------------------------------------------------------------------//
+    //Интерактивная карта
+    YMaps.jQuery(function () {
+      // Создает экземпляр карты и привязывает его к созданному контейнеру
+      var map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]);
 
-      minusAdults.addEventListener("click", function(event) {
-        event.preventDefault();
-        var value = parseInt(inputAdults.value);
-        if (value > 0){
-          value = value - 1;
-        }
-        inputAdults.value = value;
-      });
+      // Устанавливает начальные параметры отображения карты: центр карты и коэффициент масштабирования
+      map.setCenter(new YMaps.GeoPoint(30.3211, 59.9363), 16);
 
-      plusKids.addEventListener("click", function(event) {
-        event.preventDefault();
-        var value = parseInt(inputKids.value) + 1;
-        inputKids.value = value;
-      });
-
-      minusKids.addEventListener("click", function(event) {
-        event.preventDefault();
-        var value = parseInt(inputKids.value);
-        if (value > 0){
-          value = value - 1;
-        }
-        inputKids.value = value;
-      });
-
-      search.addEventListener("click", function(event) {
-        if (!inputArrivalDate.value || !inputDepartureDate.value) {
-          event.preventDefault();
-          console.log("Нужно ввести даты заезда и выезда.");
-          if (!inputArrivalDate.value){
-            inputArrivalDate.classList.add("input-block-date-empty");
-          }
-          if (!inputDepartureDate.value){
-            inputDepartureDate.classList.add("input-block-date-empty");
-          }
-         }
-      });
-
-      inputArrivalDate.addEventListener("click", function(event) {
-        inputArrivalDate.classList.remove("input-block-date-empty");
-      });
-
-      inputDepartureDate.addEventListener("click", function(event) {
-        inputDepartureDate.classList.remove("input-block-date-empty");
-      });*/
+      var s = new YMaps.Style();
+      s.iconStyle = new YMaps.IconStyle();
+      s.iconStyle.href = "../img/icon-map-pin.svg";
+      s.iconStyle.size = new YMaps.Point(66, 100);
+      s.iconStyle.offset = new YMaps.Point(-15, -90);
+      var placemark = new YMaps.Placemark(new YMaps.GeoPoint(30.3211, 59.9363), {style: s});
+      placemark.name = "Mishka";
+      placemark.description = "";
+      map.addOverlay(placemark);
+    })
